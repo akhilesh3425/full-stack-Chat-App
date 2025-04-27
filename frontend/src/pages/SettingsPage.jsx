0dbf91c81f70713840af0ccdcc783e03a44986a1
akhilesh <akhileshbutola4@gmail.com>
2025-04-27 22:19:00 +0530

    import useEffect in SettingsPage component

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,6 +1,7 @@
 import { THEMES } from "../constants";
 import { useThemeStore } from "../store/useThemeStore";
 import { Send } from "lucide-react";
+import { useEffect } from "react";
 
 const PREVIEW_MESSAGES = [
   { id: 1, content: "Hey! How's it going?", isSent: false },
